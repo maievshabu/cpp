d0f750af7bbc77e9fe2bbb7fad8bb39aa1e95fa2
fengzeng <77965984@qq.com>
2024-08-18 17:14:02 +0800

    v1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include <iostream>
+#include "A/A.cpp"
+#include "B/A.cpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+//    A::test();
+    B::test();
+    return 0;
+}
