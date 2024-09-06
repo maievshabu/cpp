a3227ead38a66700653efc55a550fbc27271301e
fengzeng <77965984@qq.com>
2024-09-06 18:04:46 +0800

    decltype

--- ch3/06/A.cpp
+++ ch3/06/A.cpp
@@ -3,10 +3,76 @@
 //
 
 #include "A.h"
+#include <typeinfo>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 namespace A06{
     int test() {
         f<A>(10);
         f<int>(1.11);
+        A a;
+        A b;
+        int c;
+        float e;
+        double f;
+
+        std::cout<< "a::" << typeid(a).name() << std::endl;
+        std::cout<< "c::" << typeid(c).name() << std::endl;
+        std::cout << typeid(a).hash_code() <<std::endl;
+        std::cout << (typeid(a) == typeid(c)  ? 1 : 0) << std::endl; // 0
+
+        auto i = c;
+        decltype(i) j = 0;
+        decltype( e + j) h;
+
+        cout << typeid(i).name() << endl; //i
+        cout << typeid(j).name() << endl; //i
+        cout << typeid(h).name() << endl;
+
+        using size_t = decltype(sizeof(0));
+        using ptrdiff_t = decltype((int*)0 - (int*)0);
+        using nullptr_t = decltype(nullptr);
+
+
+        vector<int> vc;
+
+        vc.push_back(1);
+        vc.push_back(2);
+        vc.push_back(3);
+
+        typedef decltype(vc.begin()) vctype;
+
+        cout << "-----------------------" << endl;
+        for(vctype t = vc.begin(); t < vc.end(); t++){
+            cout << *t <<endl;
+        }
+
+        cout << "-----------------------" << endl;
+        for(auto t = vc.begin(); t < vc.end(); t++){
+            cout << *t <<endl;
+        }
+
+        cout << "-----------------------" << endl;
+
+//        string who("maiev");;;;
+//
+//        decltype(anon_s) as;
+//        as[0].id.key = decltype(anon_e)::K1;
+//        as[0].id.name = who;
+//
+//        cout << "as.key::" << (as[0].id.key) << endl;
+//        cout << "as.name::" << (as[0].id.name) << endl;
+
+        int aa = 1;
+        float bb = 1.2f;
+        float cc ;
+        Sum(aa, bb, cc);
+
+        cout << cc << endl;
+
         return 0;
     }
 }
--- ch3/06/A.h
+++ ch3/06/A.h
@@ -5,16 +5,36 @@
 #ifndef UNTITLED_A_H
 #define UNTITLED_A_H
 
+#include <string>
+using namespace std;
+namespace A06 {
+    class A {
+    public:
+        typedef int foo;
+    };
 
-class A {
-public:
-    typedef int foo;
-};
+    template<typename T>
+    void f(typename T::foo a) { std::cout << "T::foo:::" << a << " ---" << hex << &a << std::endl; }
 
-template <typename T>
-void f(typename T::foo a){ std::cout<<"T::foo:::" << a <<" ---"<< hex << &a << std::endl;}
+    template<typename T>
+    void f(T a) { std::cout << "T:::" << a << "---" << hex << &a << std::endl; }
 
-template <typename T>
-void f(T a) { std::cout<< "T:::" << a << "---" << hex  << &a << std::endl;}
+//    enum class{
+//        K1, k2, k3
+//    }anon_e;
+//
+//    union{
+//        decltype(anon_e) key;
+//        string name;
+//    }anon_u;
 
+//    struct {
+//        decltype(anon_u) id;
+//    }anon_s[100];
+
+    template<typename T1, typename T2>
+    void Sum(T1 & t1, T2 & t2, decltype( t1 + t2) &s){
+        s = t1 + t2;
+    }
+}
 #endif //UNTITLED_A_H
